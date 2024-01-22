a540d71662f941179d0f750d7aec9e34234a278d
Matthew D Nolan <104527166+Mattermania@users.noreply.github.com>
2024-01-22 10:10:36 -0700

    Update MUX2x1.v

--- src/MUX2x1.v
+++ src/MUX2x1.v
@@ -3,11 +3,11 @@
 module MUX2x1(a,b,sel,d);
 
     parameter DATAWIDTH = 32;
-    input unsigned [DATAWIDTH-1:0] a;
-    input unsigned [DATAWIDTH-1:0] b;
+    input [DATAWIDTH-1:0] a;
+    input [DATAWIDTH-1:0] b;
     input sel;
 
-    output reg unsigned [DATAWIDTH-1:0] d;
+    output reg [DATAWIDTH-1:0] d;
 
     always @(a,b,sel) begin
         if (sel) begin

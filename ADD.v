cd78d1f955034d1089ced43d5374d674052d6102
Matthew D Nolan <104527166+Mattermania@users.noreply.github.com>
2024-01-21 19:16:52 -0700

    Add files via upload

---
+++ ADD.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ns
+
+module ADD(a,b,sum);
+
+    parameter DATAWIDTH = 32;
+    input signed [DATAWIDTH-1:0] a;
+    input signed [DATAWIDTH-1:0] b;
+
+    output reg signed [DATAWIDTH-1:0] sum;
+
+    always @(a,b) begin
+        sum <= a+b;
+    end
+endmodule

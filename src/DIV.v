f1143e0c281693cc0433034ac3f174f9509f434c
Matthew D Nolan <104527166+Mattermania@users.noreply.github.com>
2024-01-22 08:58:21 -0700

    Update DIV.v

--- src/DIV.v
+++ src/DIV.v
@@ -3,10 +3,10 @@
 module DIV(a,b,qout);
 
     parameter DATAWIDTH = 32;
-    input signed [DATAWIDTH-1:0] a;
-    input signed [DATAWIDTH-1:0] b;
+    input unsigned [DATAWIDTH-1:0] a;
+    input unsigned [DATAWIDTH-1:0] b;
 
-    output reg signed [DATAWIDTH-1:0] qout;
+    output reg unsigned [DATAWIDTH-1:0] qout;
 
     always @(a,b) begin
         qout <= a/b;

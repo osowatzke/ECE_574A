3b4f0a31e20d619628ef930feac4cba585867c76
Matthew D Nolan <104527166+Mattermania@users.noreply.github.com>
2024-01-22 10:11:02 -0700

    Update MOD.v

--- src/MOD.v
+++ src/MOD.v
@@ -4,10 +4,10 @@ module MOD(a, b, rem);
 
     parameter DATAWIDTH = 32;
 
-    input unsigned [DATAWIDTH-1:0] a;
-    input unsigned [DATAWIDTH-1:0] b;
+    input [DATAWIDTH-1:0] a;
+    input [DATAWIDTH-1:0] b;
     
-    output reg unsigned [DATAWIDTH-1:0] rem;
+    output reg [DATAWIDTH-1:0] rem;
     
     always @(a,b) begin
         rem <= a % b;

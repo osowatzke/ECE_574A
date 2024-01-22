0d60c9d9d1a5d5e6007b6643417d02faa5dfd2cd
Matthew D Nolan <104527166+Mattermania@users.noreply.github.com>
2024-01-22 08:55:28 -0700

    Update ADD.v

--- src/ADD.v
+++ src/ADD.v
@@ -3,10 +3,10 @@
 module ADD(a,b,sum);
 
     parameter DATAWIDTH = 32;
-    input signed [DATAWIDTH-1:0] a;
-    input signed [DATAWIDTH-1:0] b;
+    input unsigned [DATAWIDTH-1:0] a;
+    input unsigned [DATAWIDTH-1:0] b;
 
-    output reg signed [DATAWIDTH-1:0] sum;
+    output reg unsigned [DATAWIDTH-1:0] sum;
 
     always @(a,b) begin
         sum <= a+b;

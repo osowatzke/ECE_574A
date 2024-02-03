4ad2aa2850d3d20d48b3bdb0fec0582195bab5f2
Owen <osowatzke@gmail.com>
2024-02-03 13:59:58 -0700

    Saving failing circuit 3

--- src/circuit3.v
+++ src/circuit3.v
@@ -9,21 +9,15 @@ module circuit3(a, b, c, d, e, f, g, h, sa, avg, clk, rst);
     output [15:0] avg;
 
     wire   [31:0] l00, l01, l02, l03, l10, l11, l2, l2div2, l2div4, l2div8;
-    wire   [31:0] sa_32, avg_32; 
-
-    wire   [15:0] l00_16, l01_16, l02_16, l03_16; 
-
-    assign l00 = {{16{1'b0}}, l00_16};
-    assign l01 = {{16{1'b0}}, l01_16};
-    assign l02 = {{16{1'b0}}, l02_16};
-    assign l03 = {{16{1'b0}}, l03_16};
+    wire   [31:0] sa_32, avg_32;
+    
     assign sa_32 = {{24{1'b0}}, sa};
     assign avg = avg_32[15:0];
 
-    ADD    #(.DATAWIDTH(16)) ADD_1(a, b, l00_16);                     // l00 = a + b
-    ADD    #(.DATAWIDTH(16)) ADD_2(c, d, l01_16);                     // l01 = c + d
-    ADD    #(.DATAWIDTH(16)) ADD_3(e, f, l02_16);                     // l02 = e + f
-    ADD    #(.DATAWIDTH(16)) ADD_4(g, h, l03_16);                     // l03 = g + h
+    ADD    #(.DATAWIDTH(16)) ADD_1(a, b, l00);                        // l00 = a + b
+    ADD    #(.DATAWIDTH(16)) ADD_2(c, d, l01);                        // l01 = c + d
+    ADD    #(.DATAWIDTH(16)) ADD_3(e, f, l02);                        // l02 = e + f
+    ADD    #(.DATAWIDTH(16)) ADD_4(g, h, l03);                        // l03 = g + h
     ADD    #(.DATAWIDTH(32)) ADD_5(l00, l01, l10);                    // l10 = l00 + l01
     ADD    #(.DATAWIDTH(32)) ADD_6(l02, l03, l11);                    // l11 = l02 + l03
     ADD    #(.DATAWIDTH(32)) ADD_7(l10, l11, l2);                     // l2 = l10 + l11
